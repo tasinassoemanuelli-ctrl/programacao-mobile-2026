fea2a551996c00b0c5b2b78f56e3dade33ad1467
Nunu do Pinguim pingo!!! <tasinasso.emanuelli@escola.pr.gov.br>
2026-04-24 12:51:10 +0000

    Criando os primeiros containers

--- lib/paginas/aula01.dart
+++ lib/paginas/aula01.dart
@@ -9,9 +9,33 @@ class Aula01 extends StatelessWidget {
       appBar: AppBar(
         title: Text("01 -  Container"),
       ),
+      // O center centraliza o conteúdo
       body: Center(
-        child: Container(
-          child: Text("Container"),
+        // Column coloca vários elementos como filhos
+        // Organiza os elementos na vertical
+        child: Column(
+          children: [
+            // Container com largura e altura
+            Container(
+              width: double.infinity, // Retorna o valor total disponível
+              height: 100,
+              color: const Color.fromARGB(255, 255, 85, 141),
+              child: Center(child: Text("Primeiro container")),
+            ),
+            //Container com padding e margin
+            Container(
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.all(16),
+              color: Colors.blue,
+              child: const Center(
+                child: Text(
+                  "Segundo container",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        
         ),
       ),
     );
