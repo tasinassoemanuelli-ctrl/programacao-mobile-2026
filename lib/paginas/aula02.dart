764e151f84f77933b0b7f46b4696647ca7bede28
Nunu do Pinguim pingo!!! <tasinasso.emanuelli@escola.pr.gov.br>
2026-05-22 12:48:49 +0000

    Aula 08 -  Linhas e Colunas

---
+++ lib/paginas/aula02.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Aula02 extends StatelessWidget {
+  const Aula02({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aula 02 - rows e columns'),
+      ),
+      body: Center(
+        child: Text("Conteúdo da aula 02"),
+      ),
+    );
+  }
+}
--- lib/rotas/routes.dart
+++ lib/rotas/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/my_home_page.dart';
 import 'package:flutter_app/paginas/aula01.dart';
+import 'package:flutter_app/paginas/aula02.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   "/": (context) => const MyHomePage(title: 'Página Principal'),
   "/aula01": (context) => const Aula01(),
+  "/aula02": (context) => const Aula02(),
 };
